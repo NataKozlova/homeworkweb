02718e6b90b55d6c68f136289c7efa2e1e9d574f
natakozlova <natabalabanova555@yandex.ru>
2023-06-04 13:59:50 -0700

    test commit

---
+++ src/test/java/CartTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+class CallbackTest {
+    private ChromeDriver driver;
+
+    @BeforeAll
+    static void setUp() {
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\endry\\Downloads\\chromedriver_win32\\chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp2() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestSomething() {
+        driver.get("http://localhost:7777/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79081234565");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+        //String actual = driver.findElement(By.tagName("p")).getText();
+        Assertions.assertEquals(expected,actual);
+
+    }
+}
